31e4292e0b8fa19da33c6a3eb5cb4f61e6245973
ruofeng <1534668453@qq.com>
2021-05-29 10:49:38 +0800

    Add File 2105291622256578.log

---
+++ log/1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+date=`date +%y%m%d%s`
+echo $date
+file="${date}.log"
+commitTip="Add File ${file}"
+touch $file
+echo $commitTip
+chmod 0777 $file
+echo $date > "./${file}"
+git add .
+git commit -a -m"$commitTip"
+git push
+#rm -rf "./${file}"
+
---
+++ log/2.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+   n=0
+   while (($n<500))
+   do
+       sh 1.sh
+        n=$((n+1))
+       sleep 2
+   done
+
